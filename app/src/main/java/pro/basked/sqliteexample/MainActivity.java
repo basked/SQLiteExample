f931f87af8bc436bcf5cedf47b3b68fc831aca44
laravel.dev <basket.baza@gmail.com>
2019-11-30 22:22:10 +0300

    Create HomeFragment

--- app/src/main/java/pro/basked/sqliteexample/MainActivity.java
+++ app/src/main/java/pro/basked/sqliteexample/MainActivity.java
@@ -10,5 +10,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (findViewById(R.id.fragment_container) != null) {
+            return;
+        }
+        HomeFragment homeFragment = new HomeFragment();
+        getSupportFragmentManager().beginTransaction().add(R.id.fragment_container,homeFragment).commit();
     }
 }
